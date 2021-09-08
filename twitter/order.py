2bd39d960ecb0cf8ab6fd72be02ebd2267ca3774
Mashrur Khondokar <84993773+theMashrur@users.noreply.github.com>
2021-09-08 18:05:49 +0100

    added function descriptions

--- twitter/order.py
+++ twitter/order.py
@@ -3,6 +3,9 @@ from binance.enums import *
 import config
 
 def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+    """
+    Contacts the binance API to execute buy and sell orders
+    """
     client = Client(config.BINANCE_KEY, config.BINANCE_SECRET)
     try:
         print("Setting order")
@@ -14,7 +17,18 @@ def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
     return True
 
 def avg(arr, num):
+    """
+    returns average of last 'num' items of arr
+    """
     if len(arr == 0):
         return len(arr)
     else:
         return sum(arr[-num:]) / num
+
+    
+"""
+Should you not wish to use the Binance API
+here is a spare order function
+"""
+#def order():
+    #pass
