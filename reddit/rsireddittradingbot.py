8d0fb3a88b8bd35aac5ccb436001d8a2e3ce9c41
Mashrur Khondokar <84993773+theMashrur@users.noreply.github.com>
2021-09-10 14:03:38 +0100

    added short trading strategy description

--- reddit/rsireddittradingbot.py
+++ reddit/rsireddittradingbot.py
@@ -38,7 +38,7 @@ def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
         return False
     return True
 
-def Average(arr):
+def avg(arr):
     """
     Calculates the average of the last 'required' items of a list
     """
@@ -63,15 +63,16 @@ for comment in reddit.subreddit("bitcoinmarkets").stream.comments():
         elif prices[-1] != float(val):
             prices.append(float(val))
 
-        print("Total sentiment is: {0}".format(round(Average(sentimentArr))))
+        print("Total sentiment is: {0}".format(round(avg(sentimentArr))))
         print("Length of prices list is " + str(len(prices)))
         
         #get estimated RSI values based on a minimum sample size of 14
         rsi = RSIIndicator(pd.Series(prices))
         dframe = rsi.rsi()
         rsiVal = dframe.iloc[-1]
-
-        if rsiVal < 30 and len(sentimentArr) > required and round(Average(sentimentArr)) > 0.5:
+        #Trading strategy involves RSI as well. A number above 70 indicates that the stock/crypto
+        #is overbought (meaning that it would be apt to sell), while 30 indicates that it is oversold (apt to buy)
+        if rsiVal < 30 and len(sentimentArr) > required and round(avg(sentimentArr)) > 0.5:
             if in_position:
                 print("BUY ORDER BUT IN POSITION")
             else:
@@ -80,7 +81,7 @@ for comment in reddit.subreddit("bitcoinmarkets").stream.comments():
                     print("BUY ORDER")
                     in_position = True
         
-        elif rsiVal > 70 and len(sentimentArr) > required and round(Average(sentimentArr)) < -0.5:
+        elif rsiVal > 70 and len(sentimentArr) > required and round(avg(sentimentArr)) < -0.5:
             if in_position:
                 order_succeeded = order(SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL)
                 if order_succeeded:
